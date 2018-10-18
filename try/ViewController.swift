b31d81079ed99b499dfc7076662bfba50ae64802
Ahmed <ahmed@Ahmeds-iMac.local>
2018-10-18 06:25:37 -0700

    first commit maslan

--- try/ViewController.swift
+++ try/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lbl.text = "it it it"
+        print("gfsdgs")
     }
 
+    @IBAction func printy(_ sender: UITapGestureRecognizer) {
+        print(lbl.text)
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
